9e3570f4a18d8c289773318083bf7f7c1a9aed13
飘逸A186 <wlpiaoyi@gmail.com>
2024-10-11 18:52:54 +0800

    no message

--- doc/init_data.sql
+++ doc/init_data.sql
@@ -1,3 +1,3 @@
-INSERT INTO `sys_user` (`id`, `account`, `password`, `dept_id`, `status`, `is_deleted`, `create_user`, `create_dept`, `update_user`) VALUES ('1', 'admin', 'MCwCFDmNjqJJV2sUuPabsoYZxJG33HNWAhRaBfnBW/e4Xbf018ik5vWnICKheA==', '1', '1', '0', '1', '1', '1');
+INSERT INTO `sys_user` (`id`, `account`, `password`, `dept_id`, `status`, `is_deleted`, `create_user`, `create_dept`, `update_user`) VALUES ('1', 'admin', 'jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=', '1', '1', '0', '1', '1', '1');
 INSERT INTO `sys_dept` (`id`, `name`, `code`, `status`, `is_deleted`, `create_dept`, `create_user`, `update_user`) VALUES ('1', 'admin', 'admin', '1', '0', '1', '1', '1');
 INSERT INTO `sys_role` (`id`, `name`, `code`, `status`, `is_deleted`, `create_user`, `create_dept`, `update_user`) VALUES ('1', 'admin', 'admin', '1', '0', '1', '1', '1');
