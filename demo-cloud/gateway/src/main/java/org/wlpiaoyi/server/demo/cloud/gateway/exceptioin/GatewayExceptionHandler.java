c7ccee9f0545bcd78772ba7ba59ac58c3d352255
飘逸A186 <wlpiaoyi@gmail.com>
2024-11-08 13:23:59 +0800

    Gateway全局异常结构优化

--- demo-cloud/gateway/src/main/java/org/wlpiaoyi/server/demo/cloud/gateway/exceptioin/GatewayExceptionHandler.java
+++ demo-cloud/gateway/src/main/java/org/wlpiaoyi/server/demo/cloud/gateway/exceptioin/GatewayExceptionHandler.java
@@ -42,55 +42,69 @@ import java.util.Map;
 public class GatewayExceptionHandler implements ErrorWebExceptionHandler {
 
     @Override
-    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex)
-    {
-        ServerHttpResponse response = exchange.getResponse();
-
+    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
         if (exchange.getResponse().isCommitted())
         {
             return Mono.error(ex);
         }
 
         String msg;
-        int code = 500;
-        if (ex instanceof NotFoundException)
-        {
+        int code;
+        if (ex instanceof NotFoundException) {
             msg = "服务未找到";
             code = 404;
-        }
-        else if (ex instanceof ResponseStatusException)
-        {
+        } else if (ex instanceof ResponseStatusException) {
             ResponseStatusException responseStatusException = (ResponseStatusException) ex;
             msg = responseStatusException.getMessage();
             code = 401;
-        }
-        else
-        {
-            msg = "内部服务器错误";
+        } else {
+            msg = "内部服务器错误:" + ex.getMessage();
+            code = 500;
         }
 
-        log.error("[网关异常处理]请求路径:{},异常信息:{}", exchange.getRequest().getPath(), ex.getMessage());
-        return webFluxResponseWriter(response, HttpStatus.valueOf(code), msg, code);
+        log.error("[网关异常处理]请求路径:{},http状态码:{} 异常信息:{}", exchange.getRequest().getPath(), code, msg);
+        return webFluxResponseWriter(exchange, HttpStatus.valueOf(code), msg, code);
     }
 
     private static final Gson defaultGson = GsonBuilder.gsonDefault();
 
     /**
+     * <p><b>{@code @description:}</b>
      * 设置webflux模型响应
+     * </p>
      *
-     * @param response ServerHttpResponse
-     * @param status http状态码
-     * @param message 响应内容
-     * @param code 响应状态码
-     * @return Mono<Void>
+     * <p><b>@param</b> <b>exchange</b>
+     * {@link ServerWebExchange}
+     * </p>
+     *
+     * <p><b>@param</b> <b>status</b>
+     * {@link HttpStatus}
+     * http状态码
+     * </p>
+     *
+     * <p><b>@param</b> <b>message</b>
+     * {@link String}
+     * http状态码
+     * </p>
+     *
+     * <p><b>@param</b> <b>code</b>
+     * {@link int}
+     * 响应状态码
+     * </p>
+     *
+     * <p><b>{@code @date:}</b>2024/11/8 13:15</p>
+     * <p><b>{@code @return:}</b>{@link Mono< Void>}</p>
+     * <p><b>{@code @author:}</b>wlpiaoyi</p>
      */
-    private static Mono<Void> webFluxResponseWriter(ServerHttpResponse response, HttpStatus status, String message, int code) {
+    private static Mono<Void> webFluxResponseWriter(ServerWebExchange exchange, HttpStatus status, String message, int code) {
+        ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(status);
         response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        Map res = new HashMap<>();
-        res.put("code", code);
-        res.put("message", message);
-        DataBuffer dataBuffer = response.bufferFactory().wrap(defaultGson.toJson(res).getBytes());
+        Map respMap = new HashMap<>();
+        respMap.put("code", code);
+        respMap.put("message", message);
+        byte[] respBytes = defaultGson.toJson(respMap).getBytes();
+        DataBuffer dataBuffer = response.bufferFactory().wrap(respBytes);
         return response.writeWith(Mono.just(dataBuffer));
     }
 
